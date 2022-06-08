960a80e1168671e0ad59acd266f3e3d105eb4dd0
Halsey <$>
2022-06-08 18:05:42 +0800

    feat: 🚀 初始化项目配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,91 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve } from "path";
+import { wrapperEnv } from "./src/utils/getEnv";
+import { visualizer } from "rollup-plugin-visualizer";
+import { createHtmlPlugin } from "vite-plugin-html";
+import viteCompression from "vite-plugin-compression";
+import eslintPlugin from "vite-plugin-eslint";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+// @see: https://vitejs.dev/config/
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+	const env = loadEnv(mode.mode, process.cwd());
+	const viteEnv = wrapperEnv(env);
+
+	return {
+		resolve: {
+			alias: {
+				"@": resolve(__dirname, "./src")
+			}
+		},
+		// global css
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `@import "@/styles/var.scss";`
+				}
+			}
+		},
+		// server config
+		server: {
+			host: "0.0.0.0", // 服务器主机名，如果允许外部访问，可设置为"0.0.0.0"
+			port: viteEnv.VITE_PORT,
+			open: viteEnv.VITE_OPEN,
+			cors: true,
+			// https: false,
+			// 代理跨域（mock 不需要配置，这里只是个事列）
+			proxy: {
+				"/api": {
+					// target: "https://www.fastmock.site/mock/f81e8333c1a9276214bcdbc170d9e0a0", // fastmock
+					target: "https://mock.mengxuegu.com/mock/629d727e6163854a32e8307e", // easymock
+					changeOrigin: true,
+					rewrite: path => path.replace(/^\/api/, "")
+				}
+			}
+		},
+		// plugins
+		plugins: [
+			react(),
+			createHtmlPlugin({
+				inject: {
+					data: {
+						title: viteEnv.VITE_GLOB_APP_TITLE
+					}
+				}
+			}),
+			// * EsLint 报错信息显示在浏览器界面上
+			eslintPlugin(),
+			// * 是否生成包预览
+			viteEnv.VITE_REPORT && visualizer(),
+			// * gzip compress
+			viteEnv.VITE_BUILD_GZIP &&
+				viteCompression({
+					verbose: true,
+					disable: false,
+					threshold: 10240,
+					algorithm: "gzip",
+					ext: ".gz"
+				})
+		],
+		// build configure
+		build: {
+			outDir: "dist",
+			minify: "terser",
+			terserOptions: {
+				// delete console/debugger
+				compress: {
+					drop_console: viteEnv.VITE_DROP_CONSOLE,
+					drop_debugger: true
+				}
+			},
+			rollupOptions: {
+				output: {
+					// Static resource classification and packaging
+					chunkFileNames: "assets/js/[name]-[hash].js",
+					entryFileNames: "assets/js/[name]-[hash].js",
+					assetFileNames: "assets/[ext]/[name]-[hash].[ext]"
+				}
+			}
+		}
+	};
+});
