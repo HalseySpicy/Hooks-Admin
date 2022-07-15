30783724430e7b7a3802bd42dfadf1679bd5f328
weigaosS <weigaosS@users.noreply.github.com>
2022-07-15 16:30:09 +0800

    chore: add cspell for typo check

---
+++ cspell.config.js
@@ -0,0 +1,27 @@
+// @see: https://cspell.org
+
+module.exports = {
+	// 当前版本号
+	version: "0.2",
+	// 语言设置
+	language: "en",
+	// 指定一个被认为是正确的单词列表，并将出现在建议列表中。
+	words: [
+		"antd",
+		"nprogress",
+		"zhongyingwen",
+		"iconfont",
+		"persistor",
+		"immer",
+		"echarts",
+		"Echarts",
+		"gitee",
+		"juejin",
+		"contentright",
+		"screenfull",
+		"fangda",
+		"zhuti",
+		"Sider",
+		"suoxiao"
+	]
+};
