4f0fc6cc8f61522da3b79ebc7a83afc50e4ffa2f
Halsey <$>
2022-07-21 11:51:45 +0800

    docs: 📚 更新微信群二维码

--- src/api/index.ts
+++ src/api/index.ts
@@ -76,6 +76,8 @@ class RequestHttp {
 				const { response } = error;
 				NProgress.done();
 				tryHideFullScreenLoading();
+				// 请求超时单独判断，请求超时没有 response
+				if (error.message.indexOf("timeout") !== -1) message.error("请求超时，请稍后再试");
 				// 根据响应的错误状态码，做不同的处理
 				if (response) return checkStatus(response.status);
 				// 服务器结果都没有返回(可能服务器错误可能客户端断网) 断网处理:可以跳转到断网页面
